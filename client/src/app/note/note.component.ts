82cc730d73fed1ecd3730c3f2016fea391176b45
Aleksandr Abaev <lowrydertrue@gmail.com>
2018-08-08 03:24:10 +1100

    Datepicker added

--- client/src/app/note/note.component.ts
+++ client/src/app/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Input, Output, DoCheck } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { Note } from '../note';
 
 @Component({
@@ -21,11 +21,6 @@ export class NoteComponent implements OnInit {
 
 	ngOnInit() {
   	if(!this.note) this.note = {};
-  }
-
-  ngDoCheck() {
-  	// this.note.description = this.sliceDescription(this.note.description, 10);
-  	// console.log(this.note.description);
   }
 
 	deleteNote() {
@@ -51,7 +46,12 @@ export class NoteComponent implements OnInit {
 		}
 		
 		// Making description not longer than this.descMaxLength
-		this.note.description = this.note.description.slice(0, this.descMaxLength);
+		if (this.note.description 
+			&& this.note.description.length >= this.descMaxLength) {
+			this.note.description =
+				this.note.description.slice(0, this.descMaxLength);
+		}
+		
 
 		// $event.which - for FireFox
 		if($event) keyCode = $event.which || $event.keyCode;
@@ -59,5 +59,25 @@ export class NoteComponent implements OnInit {
 		if(!$event || keyCode == 13) {
 			this.onUpdateNote.emit(this.note);
 		}
+	}
+
+
+	onDateSelect($event: any): void {
+		console.log($event);
+		// Note: this should change in the near future with
+		// using native Date for all public APIs
+		// Datepicker uses NgbDateStruct as a model and not the
+		// native Date object. It's a simple data structure
+		// with 3 fields. Also note that months start with 1
+		this.note.notificationDate = new Date(
+			$event.year, $event.month - 1, $event.day);
+
+		this.updateNote();
+	}
+
+
+	onDateClear() {
+		delete this.note.notificationDate;
+		this.updateNote();
 	}
 }
