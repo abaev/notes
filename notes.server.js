b27988d29ea3a032e603d0b6cb5824f429d53765
Aleksandr Abaev <lowrydertrue@gmail.com>
2018-09-05 03:43:22 +1100

    ''

--- notes.server.js
+++ notes.server.js
@@ -55,7 +55,7 @@ app.use(function(req, res, next) {
   // res.header('Access-Control-Allow-Origin', 
   //   conf.notesUrl);
   res.header('Access-Control-Allow-Origin', 
-    conf.notesUrl);
+    '*');
   res.header('Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials',
