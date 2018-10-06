2e9dd1f9e9762033f5fa8eb31076377527fbab62
Aleksandr Abaev <lowrydertrue@gmail.com>
2018-10-07 02:10:45 +1100

    ''

--- user.controller.js
+++ user.controller.js
@@ -122,9 +122,10 @@ async function saveSubscription(req, res, next) {
 	}
 
 	// Do validation
-	if(validSubscription.validate(req.body.subscription).error) {
-		console.error(`req.body.subscription = ${JSON.stringify(req.body.subscription)}`);
-		return next({ statusCode: 400, message: 'Bad request'});
+	if(validSubscription.validate(req.body.subscription,
+			{ allowUnknown: true } ).error) {
+				console.error(`req.body.subscription = ${JSON.stringify(req.body.subscription)}`);
+				return next({ statusCode: 400, message: 'Bad request'});
 	}
 
 	try {
